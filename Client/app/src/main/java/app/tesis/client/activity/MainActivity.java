86753c08f3ff3fa8b1ec062ad2f45d4cafc7087d
Gustavo Tajes Genga <gustavo82mdq@gmail.com>
2016-08-07 17:34:04 -0300

    AS Migration

---
+++ Client/app/src/main/java/app/tesis/client/activity/MainActivity.java
@@ -0,0 +1,106 @@
+package app.tesis.client.activity;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.StrictMode;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ToggleButton;
+import app.tesis.client.R;
+import app.tesis.client.service.BoundService;
+import app.tesis.client.service.BoundService.MyLocalBinder;
+
+public class MainActivity extends Activity {
+
+    BoundService service;
+    boolean isBound = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        Intent intent = new Intent(this, BoundService.class);
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
+	}
+
+    private ServiceConnection myConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MyLocalBinder binder = (MyLocalBinder) iBinder;
+            service = binder.getService();
+            service.setContext(getApplicationContext());
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isBound = false;
+        }
+    };
+
+    public void toggleConnect(View view) {
+    	if (!this.service.isConnected()) {
+    		service.start();
+    	} else {
+    		service.stop();
+    	}
+    	((ToggleButton) view).setChecked(this.service.isConnected());
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+            Intent i = new Intent(this, AppPreferenceActivity.class);
+            startActivity(i);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+    } //end fragment
+
+}
